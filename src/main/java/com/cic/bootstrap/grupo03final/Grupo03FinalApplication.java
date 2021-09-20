18afac99901a8c0d7c27582d16699134581cf4e1
rpovedano <rpovedano@gmail.com>
2021-09-20 11:00:11 +0200

    Version inicial de spring

---
+++ src/main/java/com/cic/bootstrap/grupo03final/Grupo03FinalApplication.java
@@ -0,0 +1,13 @@
+package com.cic.bootstrap.grupo03final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo03FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo03FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cic/bootstrap/grupo03final/Grupo03FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cic.bootstrap.grupo03final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo03FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
