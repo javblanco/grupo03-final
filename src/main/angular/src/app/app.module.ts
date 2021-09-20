c4bfc731b71131d78cf4fe07e5481cb5756f3b51
rpovedano <rpovedano@gmail.com>
2021-09-20 19:19:49 +0200

    Base de datos en memoria para mockear el server hasta que esté disponible.

--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -6,17 +6,28 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { InMemoryDataService } from './service/in-memory-data.service';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+
+
+import { TipoProductoMaestroComponent } from './tipo-producto-maestro/tipo-producto-maestro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TipoProductoMaestroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+    InMemoryDataService, { dataEncapsulation: false }
+)
   ],
   providers: [],
   bootstrap: [AppComponent]
