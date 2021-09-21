7e22d4452e29bbc98036b5bc493e69c125f51543
curso10 curso10 <pedromoragolv@gmail.com>
2021-09-21 11:22:50 +0200

    Empezamos test

--- src/main/java/es/cic/bootcamp/grupo03final/controlador/TipoProductoControlador.java
+++ src/main/java/es/cic/bootcamp/grupo03final/controlador/TipoProductoControlador.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import es.cic.bootcamp.grupo03.dto.TipoProductoDto;
 import es.cic.bootcamp.grupo03final.servicio.TipoProductoServicio;
 
---
+++ src/test/java/es/cic/bootcamp/grupo03final/controller/TipoProductoControllerIntegrationTest.java
@@ -0,0 +1,5 @@
+package es.cic.bootcamp.grupo03final.controller;
+
+public class TipoProductoControllerIntegrationTest {
+
+}
